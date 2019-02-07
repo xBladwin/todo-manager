d917d21189cf257bba2f6e60bc1df5a1426cf293
Jade Baldwin <wjbaldwin171@stevenscollege.edu>
2019-02-07 15:10:07 -0500

    updates

--- manager.py
+++ manager.py
@@ -3,10 +3,23 @@ import items
 class manager(object):
 
     def read(self):
-        pass
 
-    def add(self):
-        pass
+        tasks = open('todo.txt', 'r')
+
+        read_lines = tasks.readlines()
+
+        while read_line != "":
+            lines = line.split(" - ")
+            # check to see if items are complete or not
+        tasks.close()
+
+    def add(self, item):
+
+        tasks = open('todo.txt', 'a')
+        task.append(item.task)
+        task.append(item.timestamp)
+        task.append(item.complete)
+        task.close()
 
     def complete(self):
         pass
--- todos.py
+++ todos.py
@@ -5,3 +5,16 @@ task = input('Read, Add, or Complete?: ')
 
 # TODO: create if statements to add read or complete tasks
 # TODO: when adding a new task, print to todo.txt
+
+if task == 'Read':
+    read.manager.manager()
+
+elif task == 'Add':
+    print("Name of task?")
+    name = input('-> ')
+
+
+elif task == 'Complete':
+
+else:
+    print("I don't know that")
