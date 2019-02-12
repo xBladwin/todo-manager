49eae5d1ef86b616323794a281f490af30d32ee7
Jade Baldwin <wjbaldwin171@stevenscollege.edu>
2019-02-12 14:19:39 -0500

    restart

--- manager.py
+++ manager.py
@@ -1,25 +1,34 @@
-import items
+import item
 
 class manager(object):
 
-    def read(self):
+    def read():
+        listTodos = open("todos.txt", "r")
+        print(listTodos.readlines())
+        listTodos.close()
+        return todo_start()
 
-        tasks = open('todo.txt', 'r')
-
-        read_lines = tasks.readlines()
-
-        while read_line != "":
-            lines = line.split(" - ")
-            # check to see if items are complete or not
-        tasks.close()
-
-    def add(self, item):
-
-        tasks = open('todo.txt', 'a')
-        task.append(item.task)
-        task.append(item.timestamp)
-        task.append(item.complete)
-        task.close()
-
-    def complete(self):
+    def add():
         pass
+
+    def complete():
+        pass
+
+
+def todo_start():
+    print("What would you like to do?")
+    print("1. View todo's")
+    print("2. Add a new todo")
+    print("3. Complete a todo")
+
+    entry = input('-> ')
+
+    if entry == '1':
+        return manager.read()
+    elif entry == '2':
+        return manager.add()
+    elif entry == '3':
+        return manager.complete()
+    else:
+        print("I dont know that command...")
+        return todo_start()
--- todos.py
+++ todos.py
@@ -1,20 +1,4 @@
 import manager
-import items
+import item
 
-task = input('Read, Add, or Complete?: ')
-
-# TODO: create if statements to add read or complete tasks
-# TODO: when adding a new task, print to todo.txt
-
-if task == 'Read':
-    read.manager.manager()
-
-elif task == 'Add':
-    print("Name of task?")
-    name = input('-> ')
-
-
-elif task == 'Complete':
-
-else:
-    print("I don't know that")
+manager.todo_start()
